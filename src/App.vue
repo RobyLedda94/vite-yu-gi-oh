4fb7cedf2ec82f554a0e9b90eb82c97bca759137
RobyLedda94 <robyledda01@yahoo.it>
2024-07-31 20:26:09 +0200

    [UPD] aggiustato scaffolding base

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <script>
+
+import { store } from './store.js';
+// istallato axios e lo importo
+import axios from 'axios';
+
+
 export default {
-  
+  components: {
+
+  },
+
+  // devo popolare l'array vuoto
+  created() {
+
+  },
+
+  methods: {
+    getCharactersList(){
+      axios.get(store.apiUrl).then((result) => {
+        store.charactersList = result.data.data;
+      });
+    }
+  },
+
+  data() {
+    return {
+      store,
+    }
+  }
 }
 </script>
 
 <template lang="">
-  <div>
-    
+  <div class='container'>
+      <div class='row'>
+
+      </div>
   </div>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { reactive } from 'vue';
 
-export default store = reactive({
-
+export const store = reactive({
+    apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+    // array vuoto da popolare con le carte che recupero
+    charactersList: [],
 });
