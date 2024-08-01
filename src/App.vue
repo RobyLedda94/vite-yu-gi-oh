66deaa5fc71c5ecb23247ad6d8ce350ff57f2417
RobyLedda94 <robyledda01@yahoo.it>
2024-08-01 17:05:18 +0200

    [UPD] riordinati gli elementi

--- src/App.vue
+++ src/App.vue
@@ -47,11 +47,24 @@ export default {
   
     <CharacterList />
 
-    <div class="container">
-      <div class="row">
-        <CharacterCard v-for="character in store.charactersList" :key="character.id" :character="character"/>
-      </div>
+    <main>
+      <div class="container-fluid text-center py-4">
+        <div class="row">
+            <div class="col-12">
+                <div class="btn">prova</div>
+            </div>
+        </div>
     </div>
+        <div class="container">
+          <div class="row">
+            <div class="founded-cards py-4">
+              <h2>Founded 20 cards</h2>
+          </div>
+          <CharacterCard v-for="character in store.charactersList" :key="character.id" :character="character"/>
+        </div>
+      </div>
+    </main>
+
    
   
   
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -14,28 +14,10 @@ export default {
 
 
 <template>
-    <main>
-        <div class="container-fluid text-center py-4">
-            <div class="row">
-                <div class="col-12">
-                    <div class="btn">prova</div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="founded-cards py-4">
-                    <h2>Fouded 20 cards</h2>
-                </div>
-            </div>
-        </div>
-    </main>
+
 
 </template>
 
 <style lang="scss" scoped>
-.founded-cards{
-    background-color: black;
-    color:white
-}
+
 </style>
